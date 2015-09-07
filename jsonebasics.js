aa9c0d8ee1a7b137bb89e5fc12d0b97e3ade076a
Student <student@RU-001.local>
2015-09-07 15:14:34 -0600

    first comment

---
+++ jsonebasics.js
@@ -0,0 +1,74 @@
+
+var userInput = prompt("What is your name?")
+
+alert("Hi RefactorU peeps");
+
+	console.log (userInput)
+	console.log ("Hello " + userInput)
+
+var ipaste = 1;	
+
+//#1	
+for(var i = 0; i < 501; i+=100){
+
+    console.log(i);
+}
+//#2
+for(var i = 1; i < 8; i++){
+	if (i === 1){
+		ipaste = 1
+
+	}
+	else {
+		ipaste = ipaste * 2
+	}
+
+    console.log(ipaste);
+}
+//#2
+for(var i = 1; i < 65; i*=2){
+
+    console.log(i);
+}
+// #3
+for(var i = 1; i < 4; i+=1){
+
+    console.log(i);
+    console.log(i);
+    console.log(i);
+}
+// #3
+for(var i = 1; i < 4; i++)
+{
+		
+		for (var j = 1 ; j < 4; j++)
+
+    	console.log(i);
+}
+
+for(var i = 1; i < 4; i++)
+{
+		
+		for (var j = 1 ; j < 4; j++)
+
+    	console.log(i);
+}
+//#4
+
+//#5
+
+//#6
+
+
+
+// #7
+for(var i = 0; i < 4; i++)
+{
+		
+		for (var j = 0 ; j < 4; j++)
+
+    	console.log(j);
+}
+
+
+
